f66da3505168a879c3ffc9d7acb8f29f31bd5bca
Saradhi <saradhi0001069@gmail.com>
2022-05-09 11:59:20 +0530

    adding src files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'react-slideshow-image/dist/styles.css'
+import Makeup from './makeup/Makeup';
+import Productdetailedpage from './makeup/Productdetailedpage';
+import { Routes, Route } from 'react-router-dom';
+import Skincare from './Skincare/Skincare';
+import Fragrance from './Fragrance/Fragrance';
+import Homepage from './Home page/Homepage';
+import Otp from './Otp & success/Otp';
+import Payment from './Payment/Payment'
+
+function App() {
+  return (
+    <>
+       <Homepage/>
+    </> 
+  );
+}
+
+export default App;
